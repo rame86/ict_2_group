bc606c431b2a9434de7c392fd41dbf7d324b4d61
ict <ict@DESKTOP-4KHS1QD>
2025-11-14 18:37:35 +0900

    test2삭제

--- src/main/java/com/example/Test3.java
+++ src/main/java/com/example/Test3.java
@@ -1,9 +1,10 @@
 package com.example;
 
-public class Test2 {
+public class Test3 {
 
 	public static void main(String[] args) {
-		System.out.println("test32");
+		
+		System.out.println("오늘?");
 
 	}
 
