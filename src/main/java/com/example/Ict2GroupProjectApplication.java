9fc145afde31646300ca043aa836d565876d278c
rame86 <ssrabw86@gmail.com>
2025-11-14 18:04:50 +0900

    최초설정

---
+++ src/main/java/com/example/Ict2GroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ict2GroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ict2GroupProjectApplication.class, args);
+	}
+
+}
